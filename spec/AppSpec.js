71360b7be3d42263ccc35dd876ef18af517a30d3
Gill <leapin_leprechaun@hotmail.com>
2018-01-28 15:30:46 +0000

    Initial files

---
+++ spec/AppSpec.js
@@ -0,0 +1,15 @@
+describe("Application Tests", function() {
+  var app;
+
+  beforeEach(function () {
+    app = new App();
+  })
+    // Additions 
+  describe('category of tests', function (){
+    it('individual test', function(){
+      //example expectation
+      // expect(app.myMethodName(2,2)).toEqual(4);
+    })
+  })
+
+});
---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Create a new constructor function
+var App = function () {
+	// Can add directly in the constructor
+	// this.myMethodName = function(num1, num2) {
+	//   return num1 + num2;
+	// }
+}
+
+
+// Or can add afterwards on the prototype
+// App.prototype.myMethodName = function (num1, num2) {
+// 	return num1 + num2;
+// }
+
